3d0b8d6a7dfa1417224e58f230e75b3621ec5357
Student <student@nichibi-04-no-MacBook-Air.local>
2017-09-11 15:13:05 +0900

    Initial Commit

---
+++ OuterPicture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OuterPicture
+//
+//  Created by Student on 2017/09/11.
+//  Copyright © 2017年 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
